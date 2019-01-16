be8790e3b8324640cf9e83ab2403a98382efb286
Dante Perea <lautaro.arg@gmail.com>
2019-01-16 10:21:20 +0800

    First version of new coderbunker.com landing page (structure example)

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+	
+    /*
+        Background slideshow
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    $('.section-4-container').backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+	
+});
